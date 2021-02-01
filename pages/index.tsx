9954ec8255dcc1a21008877ebfda0e28d22ed1be
Ilya Konstantinov <ilykonst95@gmail.com>
2021-02-01 23:11:50 +0400

    meta

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { Card, Container } from "../src/components";
 import styles from "./home.module.scss";
 import type { ILastnameTransitionInfo } from "../src/lastname";
 import { LastNameForm, LastnameInfoTransitionResultsCard, LastnameTransit } from "../src/lastname";
+import Head from "next/head";
 
 const Home: FC = () => {
   const [lastNameInfoArray, setLastNameInfoArray] = useState<Array<ILastnameTransitionInfo> | null>(null);
@@ -18,18 +19,27 @@ const Home: FC = () => {
     }
   }, []);
 
+  const title = "Твоя фамилия";
+
   return (
-    <Container center fullHeight>
-      <div className={styles.root}>
-        <Card>
-          <h2 className={styles.title}>Твоя фамилия</h2>
+    <>
+      <Head>
+        <title key={"title"}>{title}</title>
+        <meta key={"meta-title"} name={"title"} content={title} />
+        <meta key={"description"} name={"description"} content={"Склонение фамилии по полу"} />
+      </Head>
+      <Container center fullHeight>
+        <div className={styles.root}>
+          <Card>
+            <h2 className={styles.title}>Твоя фамилия</h2>
 
-          <LastNameForm onSubmit={onSubmit} />
-        </Card>
+            <LastNameForm onSubmit={onSubmit} />
+          </Card>
 
-        {lastNameInfoArray && <LastnameInfoTransitionResultsCard items={lastNameInfoArray} />}
-      </div>
-    </Container>
+          {lastNameInfoArray && <LastnameInfoTransitionResultsCard items={lastNameInfoArray} />}
+        </div>
+      </Container>
+    </>
   );
 };
 
