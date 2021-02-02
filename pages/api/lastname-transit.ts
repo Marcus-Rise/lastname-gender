38c400700302e69f4cb09dbf0cfe0d712c92ec85
Ilya Konstantinov <ilykonst95@gmail.com>
2021-02-02 17:17:08 +0400

    feat(/api/lastname-transit): server handler

---
+++ pages/api/lastname-transit.ts
@@ -0,0 +1,47 @@
+import type { NextApiHandler } from "next";
+import { runMiddleware } from "../../src/server";
+import Cors from "cors";
+import type { ILastnameTransitionInfo } from "../../src/lastname";
+import { Form } from "multiparty";
+
+interface IFormDataFile {
+  fieldName: string;
+  originalFilename: string;
+  path: string;
+  headers: Record<string, string>;
+  size: number;
+}
+
+const LastnameTransitHandler: NextApiHandler<ILastnameTransitionInfo[]> = async (req, res) => {
+  await runMiddleware(
+    req,
+    res,
+    Cors({
+      methods: ["POST"],
+    }),
+  );
+
+  const form = new Form();
+
+  const files = await new Promise<IFormDataFile[]>((resolve, reject) => {
+    form.parse(req, (err, fields, files: Record<string, IFormDataFile[]>) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files.file);
+      }
+    });
+  });
+
+  console.debug(files);
+
+  res.json([]);
+};
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default LastnameTransitHandler;
